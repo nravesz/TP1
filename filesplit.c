0bc677e4a2d52f8020eaa7e806cf04f82d453642
xNicky <nraveszani@gmail.com>
2017-05-21 18:22:29 -0300

    Update filesplit.c

--- filesplit.c
+++ filesplit.c
@@ -1,6 +1,8 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 char* generar_nombre(char *argv[], int contador);
 
@@ -15,29 +17,44 @@ int main(int argc, char* argv[]){
 		fprintf(stderr, "ERROR: Ha habido un problema con el archivo \"%s\"!\n", argv[1]);
 		return 0;
 	}
-	int contador  = 0;
 	int n_archivo = 1;
-	while (!feof(archivo)){
+	char* linea = NULL;
+	size_t capacidad = 0;
+	ssize_t longitud = getline(&linea, &capacidad, archivo);
+	int restantes = (int)longitud;
+	int j = 0;
+	while (longitud != -1){
 		char* nombre = generar_nombre(argv, n_archivo);
 		FILE* nuevo = fopen(nombre,"w");
-		free(nombre);
-		int c;
-		while ((c = fgetc(archivo)) != EOF){
-			if (c == '\n'){
-				fputs(" ", nuevo);
+		if (restantes > cant_caracter){ // Si la longitud es mayor que la cantidad de caracteres a imprimir
+			char cadena[cant_caracter];
+			for (int i = 0; i < cant_caracter; i++){
+				cadena[i] = linea[j];
+				j++;
+				restantes--;
 			}
-			else{
-				char buffer[1];
-				sprintf(buffer, "%c", c);
-				fputs(buffer, nuevo);
-			}
-			contador++;
-			if (contador > cant_caracter){
-				fputs("\0", nuevo);
-				contador = 0;
-				break;
-			}
+			cadena[cant_caracter] = '\0';
+			char buffer[cant_caracter];
+			sprintf(buffer, "%s", cadena);
+			fputs(buffer, nuevo);
 		}
+		else{
+			char cadena[restantes];
+			for (int i = 0; i < restantes; i++){
+				cadena[i] = linea[j];
+				j++;
+			}
+			cadena[restantes] = '\0';
+			char buffer[restantes];
+			sprintf(buffer, "%s", cadena);
+			fputs(buffer, nuevo);
+			free(linea);
+			linea = NULL;
+			longitud = getline(&linea, &capacidad, archivo);
+			restantes = (int)longitud;
+			j = 0;
+		}
+		free(nombre);
 		fclose(nuevo);
 		n_archivo++;
 	}
@@ -48,16 +65,16 @@ int main(int argc, char* argv[]){
 char* generar_nombre(char *argv[], int contador){
 	char* nombre = malloc(sizeof(char));
 	if (contador >= 1000){
-		sprintf(nombre, "%s_%i.txt", argv[1], contador);
+		sprintf(nombre, "%s_%i", argv[1], contador);
 	}
 	else if (contador >= 100){
-		sprintf(nombre, "%s_0%i.txt", argv[1], contador);
+		sprintf(nombre, "%s_0%i", argv[1], contador);
 	}
 	else if (contador >= 10){
-		sprintf(nombre, "%s_00%i.txt", argv[1], contador);
+		sprintf(nombre, "%s_00%i", argv[1], contador);
 	}
 	else{
-		sprintf(nombre, "%s_000%i.txt", argv[1], contador);
+		sprintf(nombre, "%s_000%i", argv[1], contador);
 	}
 	return nombre;
 }
